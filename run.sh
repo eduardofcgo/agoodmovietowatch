7b156d1591f3f31ba743fe6eb0b3f8094e7f0b9a
Eduardo Goncalves <eduardofcbg@gmail.com>
2023-05-31 22:14:38 +0100

    publish taxonomy

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python download.py && python database.py && python stevenlu.py && python publish.py
+python download.py && python database.py && python stevenlu.py && python taxonomy.py && python publish.py
